47d2a16ebb769db461d83fff44622ef9b0d01d76
Luka Vavetić <luka.vavetic@gmail.com>
2020-02-01 11:48:42 +0100

    Refactor user create functionality. Implement error mappings.

--- controllers/users/userscontroller.go
+++ controllers/users/userscontroller.go
@@ -2,11 +2,42 @@ package users
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/lukavavetic/bookstore-users-api/domain"
+	"github.com/lukavavetic/bookstore-users-api/services"
+	"github.com/lukavavetic/bookstore-users-api/utils/errors"
+
+	//"encoding/json"
+	//"io/ioutil"
 	"net/http"
 )
 
 func CreateUser(c *gin.Context) {
+	var user domain.User
 
+	//bytes, err := ioutil.ReadAll(c.Request.Body)
+	//if err != nil {
+	//	//handle error
+	//}
+	//
+	//if err := json.Unmarshal(bytes, &user); err != nil {
+	//	//handle error
+	//}
+
+	if err := c.ShouldBindJSON(&user); err != nil {
+		restErr := errors.BadRequest("Invalid JSON body!")
+
+		c.JSON(restErr.Status, restErr)
+		return
+	}
+
+	res, err := services.CreateUser(user)
+
+	if err != nil {
+		c.JSON(err.Status, err)
+		return
+	}
+
+	c.JSON(http.StatusCreated, res)
 }
 
 
--- services/userservice.go
+++ services/userservice.go
@@ -1,1 +1,10 @@
 package services
+
+import (
+	"github.com/lukavavetic/bookstore-users-api/domain"
+	"github.com/lukavavetic/bookstore-users-api/utils/errors"
+)
+
+func CreateUser(user domain.User) (*domain.User, *errors.RestErr) {
+	return &user, nil
+}
